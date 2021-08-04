98b38a563df716c843a08ec33cab32c47e891b7a
Federico Sismondi <federico.sismondi@camptocamp.com>
2021-08-04 14:04:23 +0200

    feat: add appSet for int and prod

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,54 +28,54 @@ module "cluster" {
         ]
       }
     },
-    # {
-    #   metadata = {
-    #     name      = "cd-int"
-    #     namespace = "argocd"
-    #   }
-    #   spec = {
-    #     description = "Demo project"
-    #     sourceRepos = ["*"]
+    {
+      metadata = {
+        name      = "argo-app-proj-int"
+        namespace = "argocd"
+      }
+      spec = {
+        description = "Demo project"
+        sourceRepos = ["*"]
 
-    #     destinations = [
-    #       {
-    #         server    = "https://kubernetes.default.svc"
-    #         namespace = "int"
-    #       }
-    #     ]
+        destinations = [
+          {
+            server    = "https://kubernetes.default.svc"
+            namespace = "int"
+          }
+        ]
 
-    #     clusterResourceWhitelist = [
-    #       {
-    #         group = ""
-    #         kind  = "Namespace"
-    #       }
-    #     ]
-    #   }
-    # },
-    # {
-    #   metadata = {
-    #     name      = "cd-prod"
-    #     namespace = "argocd"
-    #   }
-    #   spec = {
-    #     description = "Demo project"
-    #     sourceRepos = ["*"]
+        clusterResourceWhitelist = [
+          {
+            group = ""
+            kind  = "Namespace"
+          }
+        ]
+      }
+    },
+    {
+      metadata = {
+        name      = "argo-app-proj-prod"
+        namespace = "argocd"
+      }
+      spec = {
+        description = "Demo project"
+        sourceRepos = ["*"]
 
-    #     destinations = [
-    #       {
-    #         server    = "https://kubernetes.default.svc"
-    #         namespace = "prod"
-    #       }
-    #     ]
+        destinations = [
+          {
+            server    = "https://kubernetes.default.svc"
+            namespace = "prod"
+          }
+        ]
 
-    #     clusterResourceWhitelist = [
-    #       {
-    #         group = ""
-    #         kind  = "Namespace"
-    #       }
-    #     ]
-    #   }
-    # }
+        clusterResourceWhitelist = [
+          {
+            group = ""
+            kind  = "Namespace"
+          }
+        ]
+      }
+    }
   ]
 
   extra_application_sets = [
@@ -127,6 +127,134 @@ module "cluster" {
             destination = {
               server    = "https://kubernetes.default.svc"
               namespace = "dev"
+            }
+
+            syncPolicy = {
+              automated = {
+                prune    = true
+                selfHeal = false
+              }
+
+              syncOptions = [
+                "CreateNamespace=true"
+              ]
+            }
+          }
+        }
+      }
+    },
+    {
+      metadata = {
+        name      = "argo-app-set-int"
+        namespace = "argocd"
+
+        annotations = {
+          "argocd.argoproj.io/sync-options" = "SkipDryRunOnMissingResource=true"
+        }
+      }
+
+      spec = {
+        generators = [
+          {
+            git = {
+              repoURL  = "https://github.com/fsismondi/example-worflow-2-apps.git"
+              revision = "int"
+              directories = [
+                {
+                  path = "apps/*"
+                }
+              ]
+            }
+          }
+        ]
+
+        template = {
+          metadata = {
+            name = "{{path.basename}}"
+          }
+
+          spec = {
+            project = "argo-app-proj-int"
+
+            source = {
+              repoURL        = "https://github.com/fsismondi/example-worflow-2-apps.git"
+              targetRevision = "int"
+              path           = "{{path}}"
+              helm = {
+                valueFiles = [
+                  "values_common.yaml",
+                  "values_int.yaml",
+                ]
+              }
+            }
+
+            destination = {
+              server    = "https://kubernetes.default.svc"
+              namespace = "int"
+            }
+
+            syncPolicy = {
+              automated = {
+                prune    = true
+                selfHeal = false
+              }
+
+              syncOptions = [
+                "CreateNamespace=true"
+              ]
+            }
+          }
+        }
+      }
+    },
+    {
+      metadata = {
+        name      = "argo-app-set-prod"
+        namespace = "argocd"
+
+        annotations = {
+          "argocd.argoproj.io/sync-options" = "SkipDryRunOnMissingResource=true"
+        }
+      }
+
+      spec = {
+        generators = [
+          {
+            git = {
+              repoURL  = "https://github.com/fsismondi/example-worflow-2-apps.git"
+              revision = "prod"
+              directories = [
+                {
+                  path = "apps/*"
+                }
+              ]
+            }
+          }
+        ]
+
+        template = {
+          metadata = {
+            name = "{{path.basename}}"
+          }
+
+          spec = {
+            project = "argo-app-proj-prod"
+
+            source = {
+              repoURL        = "https://github.com/fsismondi/example-worflow-2-apps.git"
+              targetRevision = "prod"
+              path           = "{{path}}"
+              helm = {
+                valueFiles = [
+                  "values_common.yaml",
+                  "values_prod.yaml",
+                ]
+              }
+            }
+
+            destination = {
+              server    = "https://kubernetes.default.svc"
+              namespace = "prod"
             }
 
             syncPolicy = {
